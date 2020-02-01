8ccc8f4117e37475f7f8d3381c9ed98ec81b4fc4
4xposed <stokp1@gmail.com>
2020-02-02 00:08:23 +0100

    add package config

--- mix.exs
+++ mix.exs
@@ -5,6 +5,7 @@ defmodule Minirate.MixProject do
     [
       app: :minirate,
       description: "Small rate-limiter.",
+      package: package(),
       version: "0.1.0",
       elixir: "~> 1.8",
       build_embedded: Mix.env() == :prod,
@@ -24,6 +25,15 @@ defmodule Minirate.MixProject do
     [
       {:plug, "~> 1.0"},
       {:credo, "~> 1.1.2", only: [:dev, :test], runtime: false},
+    ]
+  end
+
+  defp package do
+    [
+      name: :minirate,
+      maintainers: ["Daniel Climent"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/4xposed/minirate"}
     ]
   end
 end
